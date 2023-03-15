da05c192ba684e102c88520263ac985f9dda251b
isademir <isademir.dev@gmail.com>
2023-03-15 22:57:42 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Bizim için veritabanı tablolarına karşılık geliyor.
+    public interface IEntity
+    {
+    }
+}
